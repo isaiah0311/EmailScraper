9aa7435b6aedadb1ea68844bfbabb8c343ced356
isaiah0311 <isaiahl0311@gmail.com>
2023-08-18 13:43:36 -0400

    Add scraping webpages for emails

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
 import requests
+import re
+
+def scrapeWebpage(response: requests.Response):
+    """Scrapes a webpage for email addresses.
+
+    Args:
+        response (requests.Response): Response to the GET request
+    
+    Returns:
+        list: List of email addressed found
+    """
+    collected = []
+
+    emails = re.findall(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)",\
+                        response.text)
+    for email in emails:
+        if collected.count(email) == 0:
+            collected.append(email)
+    
+    return collected
 
 def requestURL(url: str):
     """Sends a GET request to the target.
 
     Args:
         url (str): URL to send the GET request to
+    
+    Returns:
+        list: List of email addresses found
     """
+    emails = []
 
     try:
         response = requests.get(url)
         response.raise_for_status()
+        emails = scrapeWebpage(response)
     except Exception as exception:
         print(exception)
-    else:
-        print("Success")
+    
+    return emails
 
 def main():
     """Entry point for the script."""
     url = input("Enter target: ")
-    requestURL(url)
-
+    emails = requestURL(url)
+    for email in emails:
+        print(email)
 
 if __name__ == "__main__":
     main()
