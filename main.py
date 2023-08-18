3bd174529facf432562ed5c6a639490bc881e890
isaiah0311 <isaiahl0311@gmail.com>
2023-08-18 15:26:16 -0400

    Add crawling the target

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 import requests
 import re
+import bs4
+import urllib
 
-def scrapeWebpage(response: requests.Response):
+def scrape_links(response: requests.Response, base: str):
+    """Scrapes a webpage for links to other pages.
+
+    Args:
+        response (requests.Response): Response to the GET request
+        base (str): URL base
+    
+    Returns:
+        list: List of links found
+    """
+    collected = []
+
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+
+    anchors = soup.find_all("a")
+    for anchor in anchors:
+        href = anchor.get("href")
+        if href != None:
+            link = ""
+            if href[0] == "#":
+                continue
+            elif "http://" in href or "https://" in href:
+                link = href
+            else:
+                link = base + href
+
+            if collected.count(link) == 0:
+                collected.append(link)
+    
+    return collected
+
+def scrape_emails(response: requests.Response):
     """Scrapes a webpage for email addresses.
 
     Args:
@@ -20,7 +53,7 @@ def scrapeWebpage(response: requests.Response):
     
     return collected
 
-def requestURL(url: str):
+def request_url(url: str):
     """Sends a GET request to the target.
 
     Args:
@@ -28,23 +61,59 @@ def requestURL(url: str):
     
     Returns:
         list: List of email addresses found
+        list: List of links found
+        bool: Whether or not the script should exit
     """
     emails = []
+    links = []
+    quit = False
+
+    parts = urllib.parse.urlsplit(url)
+    base = "{0.scheme}://{0.netloc}".format(parts)
 
     try:
         response = requests.get(url)
         response.raise_for_status()
-        emails = scrapeWebpage(response)
-    except Exception as exception:
-        print(exception)
+
+        emails = scrape_emails(response)
+        links = scrape_links(response, base)
+    except KeyboardInterrupt:
+        quit = True
+    except:
+        print("ERROR: " + url)
+    else:
+        print(url)
     
-    return emails
+    return emails, links, quit
 
 def main():
     """Entry point for the script."""
-    url = input("Enter target: ")
-    emails = requestURL(url)
-    for email in emails:
+    results = []
+
+    target = input("Enter target: ")
+
+    queue = [target]
+    done = []
+
+    for url in queue:
+        emails, links, quit = request_url(queue[0])
+
+        for email in emails:
+            if results.count(email) == 0:
+                results.append(email)
+        
+        for link in links:
+            if queue.count(link) == 0 and done.count(link) == 0:
+                queue.append(link)
+
+        queue.pop(0)
+        done.append(url)
+
+        if quit:
+            break
+
+    print()
+    for email in results:
         print(email)
 
 if __name__ == "__main__":
