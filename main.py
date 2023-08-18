368fd76fe6b4a444e6c1246897063720d749e50b
isaiah0311 <isaiahl0311@gmail.com>
2023-08-18 13:14:46 -0400

    Add requesting URLs

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+import requests
+
+def requestURL(url: str):
+    """Sends a GET request to the target.
+
+    Args:
+        url (str): URL to send the GET request to
+    """
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except Exception as exception:
+        print(exception)
+    else:
+        print("Success")
+
 def main():
-    """
-    Entry point for the project.
-    """
-    print("Hello World")
+    """Entry point for the script."""
+    url = input("Enter target: ")
+    requestURL(url)
+
 
 if __name__ == "__main__":
     main()
